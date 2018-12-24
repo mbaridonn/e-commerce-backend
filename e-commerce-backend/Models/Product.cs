56a62cd14aa8ac2ca1bc15e469170d4957ac3e3f
mbaridonn <mbaridonn@gmail.com>
2018-12-24 04:20:54 -0300

    Created Product under Models

---
+++ e-commerce-backend/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_commerce_backend.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public float Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
